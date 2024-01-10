95024baa0b30d6a064f66b29f73f94574f23ed5a
OchaTEK <154081686+OchaTEK@users.noreply.github.com>
2024-01-10 05:52:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'Access the values of your ProxyOT points and lists.'
 LONG_DESCRIPTION = 'A python package that allows you access to your points and lists in ProxyOT. Using this package, you can easily access these values in your Proxyot Apps, as well as your own Python scripts.'
 
