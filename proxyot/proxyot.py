de88154be9268ef48ba19c3d17bfceb2d56a7520
OchaTEK <154081686+OchaTEK@users.noreply.github.com>
2024-01-10 05:40:58 -0500

    Update proxyot.py

--- proxyot/proxyot.py
+++ proxyot/proxyot.py
@@ -8,11 +8,19 @@ class Client():
     def __init__(self, sk):
         self.server = 'https://api.proxyot.com/'
         self.sk = sk
-
+    def write_point(self, path, value):
+        """Write a value to an existing point"""
+        p = requests.post(
+            f'{self.server}write_point', data={'path': path, 'sk': self.sk, 'value':value})
+        j = json.loads(p.text)
+        if j['error']:
+            raise Exception(j['error'])
+        else:
+            return j['value']
     def get_point(self, path):
         """Return the current value of a point."""
         p = requests.post(
-            f'{self.server}/get_point', data={'path': path, 'sk': self.sk})
+            f'{self.server}get_point', data={'path': path, 'sk': self.sk})
         j = json.loads(p.text)
         if j['error']:
             raise Exception(j['error'])
@@ -22,7 +30,7 @@ class Client():
     def get_list(self, list_name):
         """Returns a list of strings that represent the values of the points in the list."""
         r = requests.post(
-            f'{self.server}/get_list', data={'name': list_name, 'sk': self.sk})
+            f'{self.server}get_list', data={'name': list_name, 'sk': self.sk})
         j = json.loads(r.text)
         if j['error']:
             raise Exception(j['error'])
